a5637bb481944a6fe3bcf869a46f7ed2dfffcabc
Sylvain Laconi <sylvain.laconi@gmail.com>
2021-04-14 13:43:28 +0200

    components creation

---
+++ src/components/quizz/QuestionCounter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './QuestionCounter.css'
+
+const QuestionCounter = () => {
+    return (
+        <div className="question-counter">
+            <span>03/10</span>
+        </div>
+    )
+}
+
+export default QuestionCounter
