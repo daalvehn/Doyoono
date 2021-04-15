ab73121603489909ea85a21a4abc0e70ada6538f
Milad Behzadi <milad.bhzdi@gmail.com>
2021-04-15 22:10:42 +0200

    update of the NumberOfQuestion design

--- src/components/homepage/NumberOfQuestionsInput.jsx
+++ src/components/homepage/NumberOfQuestionsInput.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import './NumberOfQuestionsInput.css'
 const NumberOfQuestionsInput = () => {
     return (
-        <div className="form-control">
-            <label for="amount">Number Of Questions</label>
-            <input
-                type="number"
-                name="amount"
-                className="form-input"
-                min="1"
-                max="50"
-            />
+        <div className="nbr-qstn">
+            <div className="nbr">
+                <p>10</p>
+            </div>
+            <div className="increase-btn">
+                <a href=""></a>
+            </div>
+            <div className="decrease-btn">
+                <a href=""></a>
+            </div>
         </div>
     )
 }
