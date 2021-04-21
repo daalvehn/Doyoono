73128f100a73d95f7d6629d912b187860198c5d7
Pierre <lightpower@hotmail.fr>
2021-04-21 17:20:53 +0200

    routing on home page and score page, debug menu on side

--- src/components/homepage/GoButton.jsx
+++ src/components/homepage/GoButton.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './GoButton.css';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
 const GoButton = () => {
     return (
         <div className="cta-btn">
-          <a href="">go!</a>
+          {/* <a href="">go!</a> */}
+          <Link to="/quiz">go!</Link>
         </div>
     )
 }
--- src/components/score/RetryButton.js
+++ src/components/score/RetryButton.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 import './RetryButton.css';
 
 const RetryButton = () => {
     return (
         <div className="div-button">
-            <a href="">Retry</a> 
+            {/* <a href="">Retry</a> */}
+            <Link to="/">Retry</Link>
         </div>
     )
 }
