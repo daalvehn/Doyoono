2049febdf59eeb4449b1e6d4dd0853411a168a31
Pierre <lightpower@hotmail.fr>
2021-04-29 11:53:24 +0200

    handled class on question switch, added audio wrong sample

--- src/components/quizz/Answer.js
+++ src/components/quizz/Answer.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Answer.css'
 import useSound from 'use-sound'
 import swoosh from '../../assets/audio/swoosh.mp3'
 import correct from '../../assets/audio/correct.mp3'
+import wrong from '../../assets/audio/wrong.mp3'
 
 const Answer = ({
     answer,
@@ -14,18 +15,32 @@ const Answer = ({
     NextQuestion,
 }) => {
     const [playSwoosh, { stopSwoosh }] = useSound(swoosh, { volume: 0.05 })
-    const [playCorrect] = useSound(correct, { volume: 0.2 })
+    const playWrong = new Audio(wrong)
+    const playCorrect = new Audio(correct)
+    playWrong.volume = 0.15
+    playCorrect.volume = 0.2
     const [playerAnswerClass, setPlayerAnswerClass] = useState('')
+
+    useEffect(() => {
+        return () => {
+            setPlayerAnswerClass('')
+        }
+    }, [answer])
 
     const checkAnswer = (e) => {
         e.preventDefault()
-        playCorrect()
-        setPlayerAnswerClass(
-            answer === correctAnswer ? 'player-correct' : 'player-wrong'
-        )
         setIsAnswersReveal(true)
-        setScore(answer === correctAnswer ? score + 100 : score)
-        // setTimeout(NextQuestion, 2500)
+
+        if (answer === correctAnswer) {
+            setPlayerAnswerClass('player-correct')
+            setScore(score + 100)
+            playCorrect.play()
+        } else {
+            setPlayerAnswerClass('player-wrong')
+            playWrong.play()
+        }
+
+        setTimeout(NextQuestion, 2500)
     }
 
     return (
