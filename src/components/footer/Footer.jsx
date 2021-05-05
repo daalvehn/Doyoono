8adb797123bfc57aa90230434148f1dfdb0bca00
Pierre <lightpower@hotmail.fr>
2021-05-05 16:29:51 +0200

    added footer containing ressources & credits

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import Ressources from './Ressources'
+import Credit from './Credit'
+
+const Footer = () => {
+    return (
+        <footer>
+            <Ressources />
+            <Credit />
+        </footer>
+    )
+}
+
+export default Footer
