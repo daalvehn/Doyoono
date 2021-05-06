fbdddae87a24a0beeea2f50d47c8f3a0a44a79be
Sylvain Laconi <sylvain.laconi@gmail.com>
2021-05-06 13:59:07 +0200

    responsive score OK

--- src/components/score/ScoreHistory.js
+++ src/components/score/ScoreHistory.js
@@ -9,14 +9,14 @@ const ScoreHistory = ({ score, userName }) => {
 
     useEffect(() => {
         scoreArray.sort((a, b) => b - a)
-        scoreArray.length = 4
+        scoreArray.length = 2
         localStorage.setItem('myScore', `${score},${scoreArray.toString()}`)
         return () => {}
     }, [])
 
     return (
-        <div>
-            <p className="player-history">{userName}'s 5 best games</p>
+        <div className="history-container">
+            <p className="player-history">{userName}'s 3 best games</p>
             <ul>
                 {scoreArray.map(
                     (score, index) => score && <li key={index}>{score}</li>
